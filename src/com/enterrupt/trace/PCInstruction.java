6b0dc0bdbacc7d2269fa8fd7b00f0be7fc9a8d46
Matt Quinn <i@mattjquinn.com>
2014-01-02 10:48:03 -0500

    Handling comparison for emissions greater than 300 characters in length.

--- src/com/enterrupt/trace/PCInstruction.java
+++ src/com/enterrupt/trace/PCInstruction.java
@@ -20,6 +20,17 @@ public class PCInstruction implements IEmission {
 		if(this.instruction.equals(other.instruction)) {
 			return true;
 		}
+
+		/**
+		 * Long emissions in the trace file (i.e., calls to Fill rowsets)
+	 	 * are typically cut off around 300 characters, but I've seen variation
+	     * in this limit. Therefore, if one long instruction starts with the text
+		 * of the other or vice versa, consider the instructions to be equal.
+		 */
+		if(this.instruction.length() > 300 || other.instruction.length() > 300) {
+			return (this.instruction.startsWith(other.instruction) ||
+						other.instruction.startsWith(this.instruction));
+		}
 		return false;
 	}
 

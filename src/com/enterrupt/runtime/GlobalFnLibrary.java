022c92c1984ffc0f8374c520fb948a384dd7bb08
Matt Quinn <i@mattjquinn.com>
2014-01-10 20:53:28 -0500

    Allowing multi-dimensional arrays to be created with CreateArrayRept.

--- src/com/enterrupt/runtime/GlobalFnLibrary.java
+++ src/com/enterrupt/runtime/GlobalFnLibrary.java
@@ -110,15 +110,34 @@ public class GlobalFnLibrary {
 
 	public static void PT_CreateArrayRept() {
 
+
         List<PTType> args = Environment.getArgsFromCallStack();
 		if(args.size() != 2 || (!(args.get(1) instanceof PTInteger))) {
 			throw new EntVMachRuntimeException("Expected two args, with the second "
 				+ "being an integer.");
 		}
 
-		PTArray newArray = PTArray.getSentinel(1, args.get(0)).alloc();
-
 		int initialSize = ((PTInteger)args.get(1)).read();
+
+		/**
+		 * If the type argument passed in is an array, create a new array
+		 * reference with a dimension that exceeds the array argument's by 1.
+		 * Otherwise create a one-dimension array.
+		 */
+		PTArray newArray = null;
+		if(args.get(0) instanceof PTArray) {
+			newArray = PTArray.getSentinel(((PTArray)args.get(0)).dimensions + 1,
+				((PTArray)args.get(0)).baseType);
+		} else {
+			newArray = PTArray.getSentinel(1, args.get(0)).alloc();
+		}
+
+		/**
+		 * IMPORTANT NOTE: If the type passed to this CreateArrayRept call is
+		 * an array, each iteration of the initialization loop should insert
+		 * a reference to that array into the array being initialized. The documentation
+		 * states that this is the behavior that occurs, unless .Clone() is used.
+		 */
 		for(int i = 0; i < initialSize; i++) {
 			throw new EntVMachRuntimeException("Must support array instantiation in "+
 				"CreateArrayRept; make sure to check toString() output.");

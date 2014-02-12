a59c8022c131bf85258664d967c5f3cd3ec7497b
Matt Quinn <i@mattjquinn.com>
2014-02-12 01:41:05 -0500

    Cleaning up.

--- src/clj/main.clj
+++ src/clj/main.clj
@@ -1,11 +1,9 @@
 (ns main
   (import org.apache.logging.log4j.Logger)
-  (import org.apache.logging.log4j.LogManager)
-	(:gen-class))
+  (import org.apache.logging.log4j.LogManager))
 
 (defn -main [& args]
   (def log (. LogManager getLogger "main"))
 	(. log info "START In main...")
 	(. log warn "END In main...")
-	(println "Hello, World!")
 )

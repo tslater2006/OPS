ba036f269376c5f39786b3f4e5da2a362928ccc1
Matt Quinn <matt@mattjquinn.com>
2014-06-05 20:05:20 -0700

    Testing conversion of tabs to 2 spaces.

--- src/com/enterrupt/Main.java
+++ src/com/enterrupt/Main.java
@@ -9,53 +9,52 @@ import org.apache.logging.log4j.*;
 
 public class Main {
 
-	private static Logger log = LogManager.getLogger(Main.class.getName());
+  private static Logger log = LogManager.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		try {
+    try {
+      Runtime.getRuntime().addShutdownHook(new ENTShutdownHook());
 
-			Runtime.getRuntime().addShutdownHook(new ENTShutdownHook());
+      Environment.setSystemVar("%Component", System.getProperty("ComponentToLoad"));
+      Environment.setSystemVar("%Menu", "SA_LEARNER_SERVICES");
+      Environment.setSystemVar("%OperatorId", "KADAMS");
+      Environment.setSystemVar("%EmployeeId", "AA0001");
 
-			Environment.setSystemVar("%Component", System.getProperty("ComponentToLoad"));
-			Environment.setSystemVar("%Menu", "SA_LEARNER_SERVICES");
-			Environment.setSystemVar("%OperatorId", "KADAMS");
-			Environment.setSystemVar("%EmployeeId", "AA0001");
+      Component c = new Component(
+          (String)Environment.getSystemVar("%Component").read(), "GBL");
+      Menu m = new Menu(
+          (String)Environment.getSystemVar("%Menu").read());
 
-			Component c = new Component(
-				(String)Environment.getSystemVar("%Component").read(), "GBL");
-			Menu m = new Menu(
-				(String)Environment.getSystemVar("%Menu").read());
+      c.getListOfComponentPC();
+      c.loadAndRunRecordPConSearchRecord();
+      c.loadAndRunComponentPConSearchRecord();
+      c.fillSearchRecord();
 
-			c.getListOfComponentPC();
-			c.loadAndRunRecordPConSearchRecord();
-			c.loadAndRunComponentPConSearchRecord();
-			c.fillSearchRecord();
+      c.loadPages();
 
-			c.loadPages();
+      c.assembleComponentStructure();
+      ComponentBuffer.printStructure();
+      ComponentStructureVerifier.verify();
+      ComponentBuffer.firstPassFill();
 
-			c.assembleComponentStructure();
-			ComponentBuffer.printStructure();
-			ComponentStructureVerifier.verify();
-			ComponentBuffer.firstPassFill();
+      TraceFileVerifier.logVerificationSummary(false);
 
-			TraceFileVerifier.logVerificationSummary(false);
-
-		} catch(EntVMachRuntimeException evmre) {
-			log.fatal(evmre.getMessage(), evmre);
-			TraceFileVerifier.logVerificationSummary(true);
-			System.exit(ExitCode.ENT_VIRTUAL_MACH_RUNTIME_EXCEPTION.getCode());
-		} catch(Exception ex) {
-			log.fatal(ex.getMessage(), ex);
-			TraceFileVerifier.logVerificationSummary(true);
-			System.exit(ExitCode.GENERIC_FAILURE.getCode());
-		}
+    } catch(EntVMachRuntimeException evmre) {
+      log.fatal(evmre.getMessage(), evmre);
+      TraceFileVerifier.logVerificationSummary(true);
+      System.exit(ExitCode.ENT_VIRTUAL_MACH_RUNTIME_EXCEPTION.getCode());
+    } catch(Exception ex) {
+      log.fatal(ex.getMessage(), ex);
+      TraceFileVerifier.logVerificationSummary(true);
+      System.exit(ExitCode.GENERIC_FAILURE.getCode());
     }
+  }
 
-	private static class ENTShutdownHook extends Thread {
-		public void run() {
-			StmtLibrary.disconnect();
-			TraceFileVerifier.closeTraceFile();
-		}
-	}
+  private static class ENTShutdownHook extends Thread {
+    public void run() {
+      StmtLibrary.disconnect();
+      TraceFileVerifier.closeTraceFile();
+    }
+  }
 }

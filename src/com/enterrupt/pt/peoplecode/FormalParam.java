5262682272ddf15d4deed46bba1c4cafa4df7904
Matt Quinn <matt@mattjquinn.com>
2014-06-07 14:06:42 -0700

    Cleaning up FormalParam.java

--- src/com/enterrupt/pt/peoplecode/FormalParam.java
+++ src/com/enterrupt/pt/peoplecode/FormalParam.java
@@ -1,21 +1,28 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt.peoplecode;
 
 import com.enterrupt.types.*;
 
 public class FormalParam {
 
-	public PTType type;
-    public String id;
+  public PTType type;
+  public String id;
 
-    public FormalParam(PTType t, String i) {
-    	this.type = t;
-        this.id = i;
-    }
+  public FormalParam(PTType t, String i) {
+    this.type = t;
+    this.id = i;
+  }
 
-	public String toString() {
-		StringBuilder b = new StringBuilder(this.id);
-		b.append(":").append(this.type);
-		return b.toString();
-	}
+  public String toString() {
+    StringBuilder b = new StringBuilder(this.id);
+    b.append(":").append(this.type);
+    return b.toString();
+  }
 }
 

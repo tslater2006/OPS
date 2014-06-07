cff2851af426d07d7b9ea472f6bdd092a25d220a
Matt Quinn <matt@mattjquinn.com>
2014-06-07 15:25:00 -0700

    Cleaning up EntInterpretException.java

--- src/com/enterrupt/antlr4/EntInterpretException.java
+++ src/com/enterrupt/antlr4/EntInterpretException.java
@@ -1,10 +1,17 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.antlr4;
 
 import com.enterrupt.runtime.EntVMachRuntimeException;
 
 public class EntInterpretException extends EntVMachRuntimeException {
 
-	public EntInterpretException(String msg, String input, int lineNbr) {
-		super(msg + "; input: \"" + input + "\" on line " + lineNbr);
-	}
+  public EntInterpretException(String msg, String input, int lineNbr) {
+    super(msg + "; input: \"" + input + "\" on line " + lineNbr);
+  }
 }

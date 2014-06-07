0d9bac48ac3b508f83fe48b69ae78a48df6f5cf8
Matt Quinn <matt@mattjquinn.com>
2014-06-07 13:32:46 -0700

    Cleaning up Menu.java

--- src/com/enterrupt/pt/Menu.java
+++ src/com/enterrupt/pt/Menu.java
@@ -1,3 +1,10 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt;
 
 import java.sql.PreparedStatement;
@@ -8,36 +15,35 @@ import com.enterrupt.runtime.*;
 
 public class Menu {
 
-    public String MENUNAME;
+  public String MENUNAME;
 
-	private static Logger log = LogManager.getLogger(Menu.class.getName());
+  private static Logger log = LogManager.getLogger(Menu.class.getName());
 
-    public Menu(String menuname) {
-        this.MENUNAME = menuname;
+  public Menu(String menuname) {
+    this.MENUNAME = menuname;
 
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
 
-		try {
-	        pstmt = StmtLibrary.getPSMENUDEFN(this.MENUNAME);
-   	    	rs = pstmt.executeQuery();
-	        rs.next();      //Do nothing with record for now.
-    	    rs.close();
-        	pstmt.close();
+    try {
+      pstmt = StmtLibrary.getPSMENUDEFN(this.MENUNAME);
+      rs = pstmt.executeQuery();
+      rs.next();      //Do nothing with record for now.
+      rs.close();
+      pstmt.close();
 
-	        pstmt = StmtLibrary.getPSMENUITEM(this.MENUNAME);
-    	    rs = pstmt.executeQuery();
-			rs.next();      //Do nothing with records for now.
-
-		} catch(java.sql.SQLException sqle) {
-            log.fatal(sqle.getMessage(), sqle);
-            System.exit(ExitCode.GENERIC_SQL_EXCEPTION.getCode());
-        } finally {
-            try {
-                if(rs != null) { rs.close(); }
-                if(pstmt != null) { pstmt.close(); }
-            } catch(java.sql.SQLException sqle) {}
-        }
+      pstmt = StmtLibrary.getPSMENUITEM(this.MENUNAME);
+      rs = pstmt.executeQuery();
+      rs.next();      //Do nothing with records for now.
+    } catch(java.sql.SQLException sqle) {
+      log.fatal(sqle.getMessage(), sqle);
+      System.exit(ExitCode.GENERIC_SQL_EXCEPTION.getCode());
+    } finally {
+      try {
+        if(rs != null) { rs.close(); }
+        if(pstmt != null) { pstmt.close(); }
+      } catch(java.sql.SQLException sqle) {}
     }
+  }
 }
 

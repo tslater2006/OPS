c41c5700870559eac322c00d2d5cd1e6552b4fa3
Matt Quinn <matt@mattjquinn.com>
2014-06-07 16:11:34 -0700

    Cleaning up NoErrorTolerancePeopleCodeLexer.java

--- src/com/enterrupt/antlr4/NoErrorTolerancePeopleCodeLexer.java
+++ src/com/enterrupt/antlr4/NoErrorTolerancePeopleCodeLexer.java
@@ -1,3 +1,10 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.antlr4;
 
 import org.antlr.v4.runtime.*;
@@ -6,11 +13,11 @@ import com.enterrupt.runtime.*;
 
 public class NoErrorTolerancePeopleCodeLexer extends PeopleCodeLexer {
 
-	public NoErrorTolerancePeopleCodeLexer(CharStream input) {
-		super(input);
-	}
+  public NoErrorTolerancePeopleCodeLexer(CharStream input) {
+    super(input);
+  }
 
-	public void recover(LexerNoViableAltException e) {
-		throw new EntVMachRuntimeException(e.getMessage(), e);
-	}
+  public void recover(LexerNoViableAltException e) {
+    throw new EntVMachRuntimeException(e.getMessage(), e);
+  }
 }

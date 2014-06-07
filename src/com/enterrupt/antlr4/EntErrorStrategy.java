1be9f609d35274e52d84aebb93283634be244f9d
Matt Quinn <matt@mattjquinn.com>
2014-06-07 15:24:17 -0700

    Cleaning up EntErrorStrategy.java

--- src/com/enterrupt/antlr4/EntErrorStrategy.java
+++ src/com/enterrupt/antlr4/EntErrorStrategy.java
@@ -1,9 +1,16 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.antlr4;
 
 import org.antlr.v4.runtime.*;
 import com.enterrupt.runtime.*;
 
-/**
+/*
  * This error strategy bails out upon encountering the
  * first syntax error; no recovery is attempted. Because the
  * PeopleCode being parsed originates from the database, rather
@@ -13,19 +20,19 @@ import com.enterrupt.runtime.*;
  */
 public class EntErrorStrategy extends DefaultErrorStrategy {
 
-	@Override
-	public void recover(Parser recognizer, RecognitionException e) {
-		throw new EntVMachRuntimeException(e.getMessage(), e);
-	}
+  @Override
+  public void recover(Parser recognizer, RecognitionException e) {
+    throw new EntVMachRuntimeException(e.getMessage(), e);
+  }
 
-	// Prevents recovery from inline errors.
-	@Override
-	public Token recoverInline(Parser recognizer) throws RecognitionException {
-		InputMismatchException ime = new InputMismatchException(recognizer);
-		throw new EntVMachRuntimeException(ime.getMessage(), ime);
-	}
+  // Prevents recovery from inline errors.
+  @Override
+  public Token recoverInline(Parser recognizer) throws RecognitionException {
+    InputMismatchException ime = new InputMismatchException(recognizer);
+    throw new EntVMachRuntimeException(ime.getMessage(), ime);
+  }
 
-	// Prevents recovery from errors in subrules.
-	@Override
-	public void sync(Parser recognizer) { }
+  // Prevents recovery from errors in subrules.
+  @Override
+  public void sync(Parser recognizer) { }
 }

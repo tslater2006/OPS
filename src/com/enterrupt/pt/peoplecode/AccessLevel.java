30e020f5b6deb442ada36f64268400837e9e7404
Matt Quinn <matt@mattjquinn.com>
2014-06-07 13:53:26 -0700

    Cleaning up AccessLevel.java

--- src/com/enterrupt/pt/peoplecode/AccessLevel.java
+++ src/com/enterrupt/pt/peoplecode/AccessLevel.java
@@ -1,6 +1,13 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt.peoplecode;
 
 public enum AccessLevel {
-	PUBLIC, PRIVATE
+  PUBLIC, PRIVATE
 }
 

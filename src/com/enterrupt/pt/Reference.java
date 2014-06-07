c93d91eaf2b885ad1381c7fae2e7fc88b60287fc
Matt Quinn <matt@mattjquinn.com>
2014-06-07 13:29:56 -0700

    Cleaning up Reference.java

--- src/com/enterrupt/pt/Reference.java
+++ src/com/enterrupt/pt/Reference.java
@@ -1,44 +1,52 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt;
 
 import java.util.HashMap;
 
 public class Reference {
 
-	public String RECNAME;
-	public String REFNAME;
+  public String RECNAME;
+  public String REFNAME;
 
-	private String processedRef;
-	public boolean isRecordFieldRef = false;
+  private String processedRef;
+  public boolean isRecordFieldRef = false;
 
-	public Reference(String recname, String refname) {
+  public Reference(String recname, String refname) {
 
-		this.processedRef = "";
+    this.processedRef = "";
 
-		/**
-	     * If the RECNAME is a reserved word, replace it with the appropriate
-		 * camelcase equivalent and set the appropriate flag.
-		 */
-		boolean containsKeyword = false;
-		if(PSDefn.defnLiteralReservedWordsTable.get(recname) != null) {
-			// remember: this assigns the value (which is camelcase) rather than the key (which is upper).
-			recname = PSDefn.defnLiteralReservedWordsTable.get(recname);
-			containsKeyword = true;
-		}
+    /*
+     * If the RECNAME is a reserved word, replace it with the appropriate
+     * camelcase equivalent and set the appropriate flag.
+     */
+    boolean containsKeyword = false;
+    if(PSDefn.defnLiteralReservedWordsTable.get(recname) != null) {
+      // remember: this assigns the value (which is camelcase) rather
+      // than the key (which is upper).
+      recname = PSDefn.defnLiteralReservedWordsTable.get(recname);
+      containsKeyword = true;
+    }
 
-		this.RECNAME = recname;
-		this.REFNAME = refname;
+    this.RECNAME = recname;
+    this.REFNAME = refname;
 
-		if(this.RECNAME != null && this.RECNAME.length() > 0) {
-			this.processedRef = this.RECNAME + "." + this.REFNAME;
-			if(!containsKeyword) {
-				this.isRecordFieldRef = true;
-			}
-		} else {
-			this.processedRef = this.REFNAME;
-		}
-	}
+    if(this.RECNAME != null && this.RECNAME.length() > 0) {
+      this.processedRef = this.RECNAME + "." + this.REFNAME;
+      if(!containsKeyword) {
+        this.isRecordFieldRef = true;
+      }
+    } else {
+      this.processedRef = this.REFNAME;
+    }
+  }
 
-	public String getValue() {
-		return this.processedRef;
-	}
+  public String getValue() {
+    return this.processedRef;
+  }
 }

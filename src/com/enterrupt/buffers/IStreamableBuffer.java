63106555c131b1a9577fc3c549d61e565e344bbc
Matt Quinn <matt@mattjquinn.com>
2014-06-07 16:25:14 -0700

    Cleaning up IStreamableBuffer.java

--- src/com/enterrupt/buffers/IStreamableBuffer.java
+++ src/com/enterrupt/buffers/IStreamableBuffer.java
@@ -1,7 +1,14 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.buffers;
 
 public interface IStreamableBuffer {
 
-	public abstract IStreamableBuffer next();
-	public abstract void resetCursors();
+  public abstract IStreamableBuffer next();
+  public abstract void resetCursors();
 }

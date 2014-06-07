a614a2d0eb050b523da8d90bb5101ae7e7f9f9ab
Matt Quinn <matt@mattjquinn.com>
2014-06-07 15:04:54 -0700

    Cleaning up PeopleCodeByteStream.java

--- src/com/enterrupt/pt/peoplecode/PeopleCodeByteStream.java
+++ src/com/enterrupt/pt/peoplecode/PeopleCodeByteStream.java
@@ -1,62 +1,69 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt.peoplecode;
 
 import com.enterrupt.pt.*;
 
 public class PeopleCodeByteStream {
 
-	public PeopleCodeProg prog;
-	private StringBuilder assembledTextBuilder;
-	private int cursorPos;
+  public PeopleCodeProg prog;
+  private StringBuilder assembledTextBuilder;
+  private int cursorPos;
 
-	public PeopleCodeByteStream(PeopleCodeProg prog) {
-		this.prog = prog;
-		this.assembledTextBuilder = new StringBuilder();
-	}
+  public PeopleCodeByteStream(PeopleCodeProg prog) {
+    this.prog = prog;
+    this.assembledTextBuilder = new StringBuilder();
+  }
 
-	public void setCursorPos(int pos) {
-		this.cursorPos = pos;
-	}
+  public void setCursorPos(int pos) {
+    this.cursorPos = pos;
+  }
 
-	public void incrementCursor() {
-		this.cursorPos++;
-	}
+  public void incrementCursor() {
+    this.cursorPos++;
+  }
 
-	public void decrementCursor() {
-		this.cursorPos--;
-	}
+  public void decrementCursor() {
+    this.cursorPos--;
+  }
 
-	public int getCursorPos() {
-		return cursorPos;
-	}
+  public int getCursorPos() {
+    return cursorPos;
+  }
 
-	public int getProgLenInBytes() {
-		return this.prog.progBytes.length;
-	}
+  public int getProgLenInBytes() {
+    return this.prog.progBytes.length;
+  }
 
-	public byte readNextByte() {
-		return prog.progBytes[this.cursorPos++];
-	}
+  public byte readNextByte() {
+    return prog.progBytes[this.cursorPos++];
+  }
 
-	public byte readAhead() {
-        if(this.cursorPos >= this.prog.progBytes.length - 1) {
-            return -1;
-        }
-        return prog.progBytes[this.cursorPos];
-	}
+  public byte readAhead() {
+    if(this.cursorPos >= this.prog.progBytes.length - 1) {
+      return -1;
+    }
+    return prog.progBytes[this.cursorPos];
+  }
 
-	public Reference getMappedReference(int idx) {
-		return this.prog.progRefsTable.get(idx);
-	}
+  public Reference getMappedReference(int idx) {
+    return this.prog.progRefsTable.get(idx);
+  }
 
-	public void appendAssembledText(char c) {
+  public void appendAssembledText(char c) {
         this.assembledTextBuilder.append(c);
-	}
+  }
 
-	public void appendAssembledText(String s) {
-		this.assembledTextBuilder.append(s);
-	}
+  public void appendAssembledText(String s) {
+    this.assembledTextBuilder.append(s);
+  }
 
-	public String getAssembledText() {
-		return this.assembledTextBuilder.toString();
-	}
+  public String getAssembledText() {
+    return this.assembledTextBuilder.toString();
+  }
 }

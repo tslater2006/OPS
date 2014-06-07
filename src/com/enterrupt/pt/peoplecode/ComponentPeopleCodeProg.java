e016bab1d2c3fa04412df32314944573eb80660b
Matt Quinn <matt@mattjquinn.com>
2014-06-07 14:05:39 -0700

    Cleaning up ComponentPeopleCodeProg.java

--- src/com/enterrupt/pt/peoplecode/ComponentPeopleCodeProg.java
+++ src/com/enterrupt/pt/peoplecode/ComponentPeopleCodeProg.java
@@ -1,3 +1,10 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt.peoplecode;
 
 import java.lang.StringBuilder;
@@ -5,80 +12,80 @@ import com.enterrupt.pt.*;
 
 public class ComponentPeopleCodeProg extends PeopleCodeProg {
 
-	public String PNLGRPNAME;
-	public String MARKET;
-	public String RECNAME;
-	public String FLDNAME;
+  public String PNLGRPNAME;
+  public String MARKET;
+  public String RECNAME;
+  public String FLDNAME;
 
-	public ComponentPeopleCodeProg(String pnlgrpname, String market, String event) {
-		super();
-		this.PNLGRPNAME = pnlgrpname;
-		this.MARKET = market;
-		this.event = event;
-		this.initBindVals();
-	}
+  public ComponentPeopleCodeProg(String pnlgrpname, String market, String event) {
+    super();
+    this.PNLGRPNAME = pnlgrpname;
+    this.MARKET = market;
+    this.event = event;
+    this.initBindVals();
+  }
 
-	public ComponentPeopleCodeProg(String pnlgrpname, String market, String recname, String event) {
-		super();
-		this.PNLGRPNAME = pnlgrpname;
-		this.MARKET = market;
-		this.RECNAME = recname;
-		this.event = event;
-		this.initBindVals();
-	}
+  public ComponentPeopleCodeProg(String pnlgrpname, String market, String recname, String event) {
+    super();
+    this.PNLGRPNAME = pnlgrpname;
+    this.MARKET = market;
+    this.RECNAME = recname;
+    this.event = event;
+    this.initBindVals();
+  }
 
-	public ComponentPeopleCodeProg(String pnlgrpname, String market, String recname, String fldname, String event) {
-		super();
-		this.PNLGRPNAME = pnlgrpname;
-		this.MARKET = market;
-		this.RECNAME = recname;
-		this.FLDNAME = fldname;
-		this.event = event;
-		this.initBindVals();
-	}
+  public ComponentPeopleCodeProg(String pnlgrpname, String market, String recname, String fldname, String event) {
+    super();
+    this.PNLGRPNAME = pnlgrpname;
+    this.MARKET = market;
+    this.RECNAME = recname;
+    this.FLDNAME = fldname;
+    this.event = event;
+    this.initBindVals();
+  }
 
-	protected void initBindVals() {
+  protected void initBindVals() {
 
-		this.bindVals = new String[14];
+    this.bindVals = new String[14];
 
-		this.bindVals[0] = PSDefn.COMPONENT;
-		this.bindVals[1] = this.PNLGRPNAME;
-		this.bindVals[2] = PSDefn.MARKET;
-		this.bindVals[3] = this.MARKET;
-		for(int i = 4; i < this.bindVals.length; i+=2) {
-			this.bindVals[i] = "0";
-			this.bindVals[i+1] = PSDefn.NULL;
-		}
+    this.bindVals[0] = PSDefn.COMPONENT;
+    this.bindVals[1] = this.PNLGRPNAME;
+    this.bindVals[2] = PSDefn.MARKET;
+    this.bindVals[3] = this.MARKET;
+    for(int i = 4; i < this.bindVals.length; i+=2) {
+      this.bindVals[i] = "0";
+      this.bindVals[i+1] = PSDefn.NULL;
+    }
 
-		int idx = 4;
-		if(this.RECNAME != null) {
-			this.bindVals[idx++] = PSDefn.RECORD;
-			this.bindVals[idx++] = this.RECNAME;
+    int idx = 4;
+    if(this.RECNAME != null) {
+      this.bindVals[idx++] = PSDefn.RECORD;
+      this.bindVals[idx++] = this.RECNAME;
 
-			if(this.FLDNAME != null) {
-				this.bindVals[idx++] = PSDefn.FIELD;
-				this.bindVals[idx++] = this.FLDNAME;
-			}
-		}
+      if(this.FLDNAME != null) {
+        this.bindVals[idx++] = PSDefn.FIELD;
+        this.bindVals[idx++] = this.FLDNAME;
+      }
+    }
 
-		this.bindVals[idx++] = PSDefn.EVENT;
-		this.bindVals[idx++] = this.event;
-	}
+    this.bindVals[idx++] = PSDefn.EVENT;
+    this.bindVals[idx++] = this.event;
+  }
 
-	public String getDescriptor() {
+  public String getDescriptor() {
 
-		StringBuilder builder = new StringBuilder();
-		builder.append("ComponentPC.").append(this.PNLGRPNAME).append(".").append(this.MARKET).append(".");
+    StringBuilder builder = new StringBuilder();
+    builder.append("ComponentPC.").append(this.PNLGRPNAME).append(".").append(this.MARKET).append(".");
 
-		if(this.RECNAME != null) {
-			builder.append(this.RECNAME).append(".");
+    if(this.RECNAME != null) {
+      builder.append(this.RECNAME).append(".");
 
-			if(this.FLDNAME != null) {
-				builder.append(this.FLDNAME).append(".");
-			}
-		}
+      if(this.FLDNAME != null) {
+        builder.append(this.FLDNAME).append(".");
+      }
+    }
 
-		builder.append(this.event);
-		return builder.toString();
-	}
+    builder.append(this.event);
+    return builder.toString();
+  }
 }

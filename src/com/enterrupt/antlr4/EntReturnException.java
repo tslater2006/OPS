bb2fb2c34d20db14536915144409525c0286be2e
Matt Quinn <matt@mattjquinn.com>
2014-06-07 15:26:20 -0700

    Cleaning up EntReturnException.java

--- src/com/enterrupt/antlr4/EntReturnException.java
+++ src/com/enterrupt/antlr4/EntReturnException.java
@@ -1,10 +1,17 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.antlr4;
 
 import com.enterrupt.runtime.EntVMachRuntimeException;
 
 public class EntReturnException extends EntVMachRuntimeException {
 
-	public EntReturnException(String instructionText) {
-		super(instructionText);
-	}
+  public EntReturnException(String instructionText) {
+    super(instructionText);
+  }
 }

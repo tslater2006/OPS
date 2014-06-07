72641d55d4d8f2bc35669134de5747ec7adaa75a
Matt Quinn <matt@mattjquinn.com>
2014-06-07 13:46:01 -0700

    Cleaning up PFlag.java

--- src/com/enterrupt/pt/pages/PFlag.java
+++ src/com/enterrupt/pt/pages/PFlag.java
@@ -1,14 +1,20 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.pt.pages;
 
 public enum PFlag {
-
-	PAGE,
-	SUBPAGE,
-	SECPAGE,
-	SCROLL_START,
-	SCROLL_LVL_DECREMENT,
-	GENERIC,
-	GROUPBOX,
-	PUSHBTN_LINK,
-	END_OF_PAGE
+  PAGE,
+  SUBPAGE,
+  SECPAGE,
+  SCROLL_START,
+  SCROLL_LVL_DECREMENT,
+  GENERIC,
+  GROUPBOX,
+  PUSHBTN_LINK,
+  END_OF_PAGE
 }

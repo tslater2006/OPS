db45e39db042c35e3be04d60fb3c7d9d2d20c59d
Matt Quinn <matt@mattjquinn.com>
2014-06-07 21:27:03 -0700

    Cleaning up EmbeddedStringAssembler.java

--- src/com/enterrupt/bytecode/EmbeddedStringAssembler.java
+++ src/com/enterrupt/bytecode/EmbeddedStringAssembler.java
@@ -1,48 +1,52 @@
-package com.enterrupt.bytecode;
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+|*===---------------------------------------------------------------------===*|
+|* This file contains modified code derived from the excellent "Decode       *|
+|* PeopleCode" open source project, maintained by Erik H                     *|
+|* and available under the ISC license at                                    *|
+|* http://sourceforge.net/projects/decodepcode/. The associated              *|
+|* license text has been reproduced here in accordance with                  *|
+|* the license requirements:                                                 *|
+|* ------------------------------------------------------------------------- *|
+|* Copyright (c)2011 Erik H (erikh3@users.sourceforge.net)                   *|
+|*                                                                           *|
+|* Permission to use, copy, modify, and/or distribute this software for any  *|
+|* purpose with or without fee is hereby granted, provided that the above    *|
+|* copyright notice and this permission notice appear in all copies.         *|
+|*                                                                           *|
+|* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  *|
+|* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF          *|
+|* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR   *|
+|* ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES    *|
+|* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN     *|
+|* ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF   *|
+|* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.            *|
+\*===---------------------------------------------------------------------===*/
 
-/**
- * ============================================================
- * This file contains source code derived from the excellent
- * Decode PeopleCode open source project, maintained by Erik H
- * and available under the ISC license at
- * http://sourceforge.net/projects/decodepcode/. The associated
- * license text has been reproduced here in accordance with
- * the license requirements.
- * ============================================================
- * Copyright (c)2011 Erik H (erikh3@users.sourceforge.net)
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+package com.enterrupt.bytecode;
 
 import com.enterrupt.pt.peoplecode.PeopleCodeByteStream;
 
 public class EmbeddedStringAssembler extends PureStringAssembler {
 
-	String pre, post;
+  String pre, post;
 
-	public EmbeddedStringAssembler(byte _b, String _pre, String _post, int _format) {
-		super(_b);
-		this.pre = _pre;
-		this.post = _post;
-		this.format = _format;
-	}
+  public EmbeddedStringAssembler(byte _b, String _pre, String _post, int _format) {
+    super(_b);
+    this.pre = _pre;
+    this.post = _post;
+    this.format = _format;
+  }
 
-	public EmbeddedStringAssembler(byte _b, String _pre, String _post) {
-		this(_b, _pre, _post, AFlag.SPACE_BEFORE_AND_AFTER);
-	}
+  public EmbeddedStringAssembler(byte _b, String _pre, String _post) {
+    this(_b, _pre, _post, AFlag.SPACE_BEFORE_AND_AFTER);
+  }
 
-	public void assemble(PeopleCodeByteStream stream) {
-		stream.appendAssembledText(this.pre
-			+ getString(stream).replace("\"", "\"\"") + this.post);
-	}
+  public void assemble(PeopleCodeByteStream stream) {
+    stream.appendAssembledText(this.pre
+        + getString(stream).replace("\"", "\"\"") + this.post);
+  }
 }

dc26cb932a5c9da526aa71ab036385a02424e01f
Matt Quinn <matt@mattjquinn.com>
2014-06-07 20:57:24 -0700

    Cleaning up RecordFieldBuffer.java

--- src/com/enterrupt/buffers/RecordFieldBuffer.java
+++ src/com/enterrupt/buffers/RecordFieldBuffer.java
@@ -1,3 +1,10 @@
+/*===---------------------------------------------------------------------===*\
+|*                       The OpenPplSoft Runtime Project                     *|
+|*                                                                           *|
+|*              This file is distributed under the MIT License.              *|
+|*                         See LICENSE.md for details.                       *|
+\*===---------------------------------------------------------------------===*/
+
 package com.enterrupt.buffers;
 
 import java.util.*;
@@ -5,73 +12,70 @@ import java.lang.StringBuilder;
 import com.enterrupt.pt.*;
 import com.enterrupt.runtime.*;
 
-public class RecordFieldBuffer implements IStreamableBuffer, Comparable<RecordFieldBuffer> {
+public class RecordFieldBuffer implements IStreamableBuffer,
+    Comparable<RecordFieldBuffer> {
 
-    public String fldName;
-    public Record recDefn;
-    public RecordField fldDefn;
-    public RecordBuffer parentRecordBuffer;
+  public String fldName;
+  public Record recDefn;
+  public RecordField fldDefn;
+  public RecordBuffer parentRecordBuffer;
 
-	// Used for reading.
-	private boolean hasEmittedSelf = false;
+  // Used for reading.
+  private boolean hasEmittedSelf = false;
 
-    public RecordFieldBuffer(String r, String f, RecordBuffer parent) {
-        this.fldName = f;
-        this.recDefn = DefnCache.getRecord(r);
-        this.fldDefn = this.recDefn.fieldTable.get(this.fldName);
-        this.parentRecordBuffer = parent;
+  public RecordFieldBuffer(String r, String f, RecordBuffer parent) {
+    this.fldName = f;
+    this.recDefn = DefnCache.getRecord(r);
+    this.fldDefn = this.recDefn.fieldTable.get(this.fldName);
+    this.parentRecordBuffer = parent;
 
-        if(this.fldDefn == null) {
-			throw new EntVMachRuntimeException("Field not found on the record supplied. Likely on a subrecord. " +
-				"Subrecord traversal in RecordFieldBuffer not supported at this time.");
-        }
+    if(this.fldDefn == null) {
+      throw new EntVMachRuntimeException("Field not found on the record supplied. Likely on a subrecord. " +
+          "Subrecord traversal in RecordFieldBuffer not supported at this time.");
+    }
+  }
+
+  public void checkFieldBufferRules() {
+
+    /*
+     * If a level 0, non-derived record contains at least one field
+     * that is neither a search key nor an alternate key, all of the record's fields
+     * should be present in the component buffer.
+     */
+    if(this.parentRecordBuffer.scrollLevel == 0
+        && !this.recDefn.isDerivedWorkRecord()
+        && !this.fldDefn.isAlternateSearchKey()) {
+      this.parentRecordBuffer.expandEntireRecordIntoBuffer();
     }
 
-    public void checkFieldBufferRules() {
+    /*
+     * All the fields on a primary scroll record at level 1 or higher
+     * should be present in the component buffer.
+     */
+    if(this.parentRecordBuffer.scrollLevel > 0
+        && this.parentRecordBuffer.isPrimaryScrollRecordBuffer) {
+      this.parentRecordBuffer.expandEntireRecordIntoBuffer();
+    }
+  }
 
-        /**
-         * If a level 0, non-derived record contains at least one field
-         * that is neither a search key nor an alternate key, all of the record's fields
-         * should be present in the component buffer.
-         */
-        if(this.parentRecordBuffer.scrollLevel == 0
-            && !this.recDefn.isDerivedWorkRecord()
-            && !this.fldDefn.isAlternateSearchKey()) {
+  public int compareTo(RecordFieldBuffer fb) {
+    int a = this.fldDefn.FIELDNUM;
+    int b = fb.fldDefn.FIELDNUM;
+    return a > b ? +1 : a < b ? -1 : 0;
+  }
 
-            this.parentRecordBuffer.expandEntireRecordIntoBuffer();
-        }
+  public IStreamableBuffer next() {
 
-        /**
-         * All the fields on a primary scroll record at level 1 or higher
-         * should be present in the component buffer.
-         */
-        if(this.parentRecordBuffer.scrollLevel > 0
-            && this.parentRecordBuffer.isPrimaryScrollRecordBuffer) {
-
-            this.parentRecordBuffer.expandEntireRecordIntoBuffer();
-        }
+    if(!this.hasEmittedSelf) {
+      this.hasEmittedSelf = true;
+      return this;
     }
 
-    public int compareTo(RecordFieldBuffer fb) {
+    return null;
+  }
 
-        int a = this.fldDefn.FIELDNUM;
-        int b = fb.fldDefn.FIELDNUM;
-        return a > b ? +1 : a < b ? -1 : 0;
-    }
-
-    public IStreamableBuffer next() {
-
-		if(!this.hasEmittedSelf) {
-			this.hasEmittedSelf = true;
-			return this;
-		}
-
-		return null;
-    }
-
-    public void resetCursors() {
-
-		this.hasEmittedSelf = false;
-    }
+  public void resetCursors() {
+    this.hasEmittedSelf = false;
+  }
 }
 

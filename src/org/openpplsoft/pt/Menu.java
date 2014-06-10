c8c037f1a5f7daa8b0fbbf92422f89ded6a56965
Matt Quinn <matt@mattjquinn.com>
2014-06-10 06:48:42 -0700

    Menu.java now passes checkstyle audit.

--- src/org/openpplsoft/pt/Menu.java
+++ src/org/openpplsoft/pt/Menu.java
@@ -9,40 +9,55 @@ package org.openpplsoft.pt;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import org.openpplsoft.sql.StmtLibrary;
-import org.apache.logging.log4j.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.openpplsoft.runtime.*;
+import org.openpplsoft.sql.StmtLibrary;
 
+/**
+ * Represents a PeopleTools menu definition.
+ */
 public class Menu {
-
-  public String MENUNAME;
 
   private static Logger log = LogManager.getLogger(Menu.class.getName());
 
-  public Menu(String menuname) {
-    this.MENUNAME = menuname;
+  private String ptMENUNAME;
+
+  /**
+   * Create a representation of the menu defn with the
+   * provided name.
+   * @param menuname name of the menu defn
+   */
+  public Menu(final String menuname) {
+    this.ptMENUNAME = menuname;
 
     PreparedStatement pstmt = null;
     ResultSet rs = null;
 
     try {
-      pstmt = StmtLibrary.getPSMENUDEFN(this.MENUNAME);
+      pstmt = StmtLibrary.getPSMENUDEFN(this.ptMENUNAME);
       rs = pstmt.executeQuery();
-      rs.next();      //Do nothing with record for now.
+      //Do nothing with record for now.
+      rs.next();
       rs.close();
       pstmt.close();
 
-      pstmt = StmtLibrary.getPSMENUITEM(this.MENUNAME);
+      pstmt = StmtLibrary.getPSMENUITEM(this.ptMENUNAME);
       rs = pstmt.executeQuery();
-      rs.next();      //Do nothing with records for now.
-    } catch(java.sql.SQLException sqle) {
+      //Do nothing with records for now.
+      rs.next();
+    } catch (final java.sql.SQLException sqle) {
       log.fatal(sqle.getMessage(), sqle);
       System.exit(ExitCode.GENERIC_SQL_EXCEPTION.getCode());
     } finally {
       try {
-        if(rs != null) { rs.close(); }
-        if(pstmt != null) { pstmt.close(); }
-      } catch(java.sql.SQLException sqle) {}
+        if (rs != null) { rs.close(); }
+        if (pstmt != null) { pstmt.close(); }
+      } catch (final java.sql.SQLException sqle) {
+        log.warn("Unable to close rs and/or pstmt in finally block.");
+      }
     }
   }
 }

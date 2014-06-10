17d77cb091f5662f6c61938dc7428fc5a1cd5401
Matt Quinn <matt@mattjquinn.com>
2014-06-09 21:26:10 -0700

    Cleaning up / documenting Scope.java

--- src/org/openpplsoft/runtime/Scope.java
+++ src/org/openpplsoft/runtime/Scope.java
@@ -7,12 +7,21 @@
 
 package org.openpplsoft.runtime;
 
-import java.util.*;
-import org.openpplsoft.types.*;
-import org.openpplsoft.runtime.*;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.openpplsoft.types.*;
+
+/**
+ * Represents a set of variable identifiers for a specific
+ * scope level within a PeopleCode program.
+ */
 public class Scope {
 
+  /**
+   * Enumerates the various scope levels available
+   * in a PeopleCode program.
+   */
   public enum Lvl {
     GLOBAL, COMPONENT,
     PROGRAM_LOCAL, FUNCTION_LOCAL, METHOD_LOCAL,
@@ -22,36 +31,63 @@ public class Scope {
   private Scope.Lvl level;
   private Map<String, PTType> symbolTable;
 
-  public Scope(Scope.Lvl l) {
+  /**
+   * Creates a new scope for the specified scope level.
+   * @param l the scoping level represented by this object
+   */
+  public Scope(final Scope.Lvl l) {
     this.level = l;
     this.symbolTable = new HashMap<String, PTType>();
   }
 
-  public void declareVar(String id, PTType ptr) {
-    if(this.isIdResolvable(id)) {
-      throw new EntVMachRuntimeException("Encountered attempt to re-declare " +
-        " variable (" + id + ") in scope level " + this.level);
+  /**
+   * Declares a variable in this scope attached to the provided identifier.
+   * If the variable has already been declared, an RTE will be thrown.
+   * @param id the identifier to declare
+   * @param type the type to declare
+   */
+  public void declareVar(final String id, final PTType type) {
+    if (this.isIdResolvable(id)) {
+      throw new EntVMachRuntimeException("Encountered attempt to re-declare "
+          + " variable (" + id + ") in scope level " + this.level);
     }
-    this.symbolTable.put(id, ptr);
+    this.symbolTable.put(id, type);
   }
 
-  public void assignVar(String id, PTType newRef) {
-    PTType currRef = this.symbolTable.get(id);
+  /**
+   * Assigns a value to the variable attached to the provided identifier.
+   * If the variable has not been declared, an RTE will be thrown.
+   * @param id the identifier to assign to
+   * @param newRef the reference to assign
+   */
+  public void assignVar(final String id, final PTType newRef) {
+    final PTType currRef = this.symbolTable.get(id);
 
-    if(currRef.typeCheck(newRef)) {
+    if (currRef.typeCheck(newRef)) {
       this.symbolTable.put(id, newRef);
     } else {
-      throw new EntVMachRuntimeException("Identifier assignment failed type " +
-        "check; currRef (" + currRef.toString() + ") and newRef (" +
-        newRef.toString() + ") are not type compatible.");
+      throw new EntVMachRuntimeException("Identifier assignment failed type "
+          + "check; currRef (" + currRef.toString() + ") and newRef ("
+          + newRef.toString() + ") are not type compatible.");
     }
   }
 
-  public PTType resolveVar(String id) {
+  /**
+   * Retrieves the current reference assigned to the provided identifier.
+   * @param id the identifier to resolve
+   * @return the current reference if declared, otherwise null
+   */
+  public PTType resolveVar(final String id) {
     return this.symbolTable.get(id);
   }
 
-  public boolean isIdResolvable(String id) {
+  /**
+   * Determines if the provided identifier has been declared in this scope.
+   * @param id the identifier to check
+   * @return true if the identifier has been declared in this scope,
+        false otherwise.
+   */
+  public boolean isIdResolvable(final String id) {
     return this.symbolTable.containsKey(id);
   }
 }

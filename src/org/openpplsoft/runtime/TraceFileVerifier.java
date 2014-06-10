befb291d2043d4288ae93564e3db3fbd8f0fd0ba
Matt Quinn <matt@mattjquinn.com>
2014-06-09 21:05:32 -0700

    Cleaning up TraceFileVerifier (passes checkstyle except for inner assignment violations).

--- src/org/openpplsoft/runtime/TraceFileVerifier.java
+++ src/org/openpplsoft/runtime/TraceFileVerifier.java
@@ -25,6 +25,12 @@ import org.apache.logging.log4j.Logger;
 import org.openpplsoft.sql.*;
 import org.openpplsoft.trace.*;
 
+/**
+ * Accepts emissions from the OPS runtime and verifies
+ * them against a PS tracefile; the exact tracefile is
+ * determined by the value of system properties passed
+ * to OPS at execution.
+ */
 public final class TraceFileVerifier {
 
   private static List<IEmission> unenforcedEmissions;
@@ -38,6 +44,12 @@ public final class TraceFileVerifier {
   private static int coverageAreaStartLineNbr, coverageAreaEndLineNbr;
   private static int numEnforcedSQLEmissions, numPCEmissionMatches;
   private static int numTraceSQLStmts, numTraceSQLStmtsIgnored;
+
+  // Define regex group indices here rather than as magic nbrs.
+  private static final int GROUP1 = 1;
+  private static final int GROUP2 = 2;
+  private static final int GROUP3 = 3;
+  private static final int GROUP4 = 4;
 
   private static Logger log = LogManager.getLogger(
       TraceFileVerifier.class.getName());
@@ -77,6 +89,14 @@ public final class TraceFileVerifier {
 
   private TraceFileVerifier() {}
 
+  /**
+   * Some SQL statements in the PS tracefile are not issued by OPS,
+   * either b/c they're unnecessary/redundant or relate to functionality
+   * not yet supported. Passing a file containing a list of SQL statements
+   * to this method will cause the verifier to ignore them if they are found
+   * in the tracefile.
+   * @param filename name of file in classpath containing SQL stmts to ignore.
+   */
   public static void ignoreStmtsInFile(final String filename) {
     try {
       final BufferedReader ignoreFileReader = new BufferedReader(new FileReader(
@@ -96,18 +116,28 @@ public final class TraceFileVerifier {
     }
   }
 
-  public static void submitUnenforcedEmission(final IEmission evmEmission) {
-    //log.debug(evmEmission);
-    unenforcedEmissions.add(evmEmission);
+  /**
+   * The verifier will not fail if it is passed an emission that does not
+   * correspond to the next emission in the PS tracefile via this method.
+   * @param opsEmission emission from OPS that should not be enforced
+   */
+  public static void submitUnenforcedEmission(final IEmission opsEmission) {
+    //log.debug(opsEmission);
+    unenforcedEmissions.add(opsEmission);
   }
 
-  public static void enforceEmission(final IEmission evmEmission) {
-    //if(!(evmEmission instanceof ENTStmt)) {
-    log.debug(evmEmission);
+  /**
+   * The verifier WILL fail if it is passed an emission that does not
+   * correspond to the next emission in the PS tracefile via this method.
+   * @param opsEmission emission from OPS that SHOULD be enforced
+   */
+  public static void enforceEmission(final IEmission opsEmission) {
+    //if(!(opsEmission instanceof ENTStmt)) {
+    log.debug(opsEmission);
     //}
 
     IEmission traceEmission;
-    if(coverageAreaStartLineNbr == 0) {
+    if (coverageAreaStartLineNbr == 0) {
     /*
      * If this is the first emission being matched, seek
      * to the match in the trace file. It is assumed that the first
@@ -117,10 +147,10 @@ public final class TraceFileVerifier {
      */
     do {
       traceEmission = getNextTraceEmission();
-    } while(traceEmission != null &&
-      !evmEmission.equals(traceEmission));
+    } while (traceEmission != null
+          && !opsEmission.equals(traceEmission));
 
-      if(traceEmission != null) {
+      if (traceEmission != null) {
         coverageAreaStartLineNbr = currTraceLineNbr;
         numEnforcedSQLEmissions++;
         return;
@@ -129,22 +159,22 @@ public final class TraceFileVerifier {
       traceEmission = getNextTraceEmission();
     }
 
-    if(traceEmission == null) {
-      throw new EntVMachRuntimeException("Reached EOF of trace file; no " +
-          "emission match is possible.");
+    if (traceEmission == null) {
+      throw new EntVMachRuntimeException("Reached EOF of trace file; no "
+          + "emission match is possible.");
     }
 
-    if(evmEmission.equals(traceEmission)) {
+    if (opsEmission.equals(traceEmission)) {
 
       // Increment emission-specific counter.
-      if(evmEmission instanceof ENTStmt) {
+      if (opsEmission instanceof ENTStmt) {
         numEnforcedSQLEmissions++;
       } else {
         numPCEmissionMatches++;
       }
     } else {
       log.fatal("=== Emission Mismatch! =======================");
-      log.fatal("OPS emitted: {}", evmEmission);
+      log.fatal("OPS emitted: {}", opsEmission);
       log.fatal("Trace file expects: {}", traceEmission);
       throw new EntVMachRuntimeException("Emission mismatch.");
     }
@@ -155,62 +185,66 @@ public final class TraceFileVerifier {
     // do-while because we want to check the line returned from the prior call
     // to getNextTraceLine().
     do {
-      Matcher sqlMatcher = sqlTokenPattern.matcher(currTraceLine);
-      if(sqlMatcher.find()) {
+      final Matcher sqlMatcher = sqlTokenPattern.matcher(currTraceLine);
+      if (sqlMatcher.find()) {
         numTraceSQLStmts++;
 
-        PSStmt ps_stmt = new PSStmt(sqlMatcher.group(1), currTraceLineNbr);
+        final PSStmt psStmt = new PSStmt(sqlMatcher.group(1), currTraceLineNbr);
 
         // If the stmt is in the ignored file, skip it.
-        if(ignoredStmts.containsKey(ps_stmt.originalStmt)) {
+        if (ignoredStmts.containsKey(psStmt.originalStmt)) {
           numTraceSQLStmtsIgnored++;
           continue;
         }
 
         // Keep reading lines for bind values.
-        while((currTraceLine = getNextTraceLine()) != null) {
-          Matcher bindValMatcher = bindValPattern.matcher(currTraceLine);
-          if(bindValMatcher.find()) {
-            ps_stmt.bindVals.put(Integer.parseInt(bindValMatcher.group(1)),
-                bindValMatcher.group(2));
+        while ((currTraceLine = getNextTraceLine()) != null) {
+          final Matcher bindValMatcher = bindValPattern.matcher(currTraceLine);
+          if (bindValMatcher.find()) {
+            psStmt.bindVals.put(Integer.parseInt(bindValMatcher
+                .group(GROUP1)), bindValMatcher.group(GROUP2));
             } else {
-              return ps_stmt; // statement has one or more bind values.
+              // statement has one or more bind values.
+              return psStmt;
             }
           }
-          return ps_stmt; // statement has no bind values.
+          // statement has no bind values.
+          return psStmt;
         }
 
-        Matcher pcStartMatcher = pcStartPattern.matcher(currTraceLine);
-        if(pcStartMatcher.find()) {
+        final Matcher pcStartMatcher = pcStartPattern.matcher(currTraceLine);
+        if (pcStartMatcher.find()) {
           // We don't want the next call to check this line again.
           currTraceLine = getNextTraceLine();
-          return new PCStart(pcStartMatcher.group(1), pcStartMatcher.group(2),
-              pcStartMatcher.group(3), pcStartMatcher.group(4));
+          return new PCStart(pcStartMatcher.group(GROUP1),
+              pcStartMatcher.group(GROUP2), pcStartMatcher.group(GROUP3),
+              pcStartMatcher.group(GROUP4));
         }
 
-        Matcher pcBeginMatcher = pcBeginPattern.matcher(currTraceLine);
-        if(pcBeginMatcher.find()) {
+        final Matcher pcBeginMatcher = pcBeginPattern.matcher(currTraceLine);
+        if (pcBeginMatcher.find()) {
           // We don't want the next call to check this line again.
           currTraceLine = getNextTraceLine();
-          return new PCBegin(pcBeginMatcher.group(1), pcBeginMatcher.group(2),
-              pcBeginMatcher.group(3));
+          return new PCBegin(pcBeginMatcher.group(GROUP1),
+              pcBeginMatcher.group(GROUP2), pcBeginMatcher.group(GROUP3));
         }
 
-        Matcher pcInstrMatcher = pcInstrPattern.matcher(currTraceLine);
-        if(pcInstrMatcher.find()) {
+        final Matcher pcInstrMatcher = pcInstrPattern.matcher(currTraceLine);
+        if (pcInstrMatcher.find()) {
           // We don't want the next call to check this line again.
           currTraceLine = getNextTraceLine();
-          return new PCInstruction(pcInstrMatcher.group(1));
+          return new PCInstruction(pcInstrMatcher.group(GROUP1));
         }
 
-        Matcher pcEndMatcher = pcEndPattern.matcher(currTraceLine);
-        if(pcEndMatcher.find()) {
+        final Matcher pcEndMatcher = pcEndPattern.matcher(currTraceLine);
+        if (pcEndMatcher.find()) {
           // We don't want the next call to check this line again.
           currTraceLine = getNextTraceLine();
-          return new PCEnd(pcEndMatcher.group(1), pcEndMatcher.group(2),
-          pcEndMatcher.group(3), pcEndMatcher.group(4));
+          return new PCEnd(pcEndMatcher.group(GROUP1),
+              pcEndMatcher.group(GROUP2), pcEndMatcher.group(GROUP3),
+              pcEndMatcher.group(GROUP4));
         }
-      } while((currTraceLine = getNextTraceLine()) != null);
+      } while ((currTraceLine = getNextTraceLine()) != null);
       return null;
     }
 
@@ -220,46 +254,60 @@ public final class TraceFileVerifier {
     try {
       line = traceFileReader.readLine();
       currTraceLineNbr++;
-    } catch(java.io.IOException ioe) {
+    } catch (final java.io.IOException ioe) {
       log.fatal(ioe.getMessage(), ioe);
       System.exit(ExitCode.FAILED_READ_FROM_TRACE_FILE.getCode());
     }
     return line;
   }
 
+  /**
+   * External code should call this method to signal to the verifier
+   * that no more emissions will be emitted.
+   */
   public static void closeTraceFile() {
     try {
       traceFileReader.close();
-    } catch(java.io.IOException ioe) {
+    } catch (final java.io.IOException ioe) {
       log.warn("Encountered IOException while attempting to close trace file.");
     }
   }
 
-  public static void logVerificationSummary(boolean mismatchFlag) {
+  /**
+   * Outputs the verifier's info collected during execution. This method
+   * should be called after all processing is complete, or if the OPS
+   * runtime encounters an exception before successful completion, it
+   * should be called prior to exiting.
+   * @param mismatchFlag true if execution completed successfully,
+   *    false otherwise
+   */
+  public static void logVerificationSummary(final boolean mismatchFlag) {
 
-    if(!mismatchFlag) {
+    if (!mismatchFlag) {
       coverageAreaEndLineNbr = currTraceLineNbr;
     }
 
     log.info("==============================================================");
     log.info("[{}] Trace File Verification Summary",
-      mismatchFlag ? "FAILURE" : "SUCCESS");
+        mismatchFlag ? "FAILURE" : "SUCCESS");
     log.info("==============================================================");
     log.info("Trace lines seen:\t\t\t{}", currTraceLineNbr);
     log.info("SQL Stmts in Ignore File:\t\t{}", ignoredStmts.size());
     log.info("SQL Stmts Seen (Total / Ignored):\t{}\t\t{}", numTraceSQLStmts,
-      numTraceSQLStmtsIgnored);
+        numTraceSQLStmtsIgnored);
     log.info("Adhoc SQL Emissions:\t\t\t{}", unenforcedEmissions.size());
     log.info("Enforced SQL Emissions:\t\t\t{}", numEnforcedSQLEmissions);
     log.info("Enforced PC Emissions:\t\t\t{}", numPCEmissionMatches);
     log.info("Total Emissions:\t\t\t\t{}",
-      numEnforcedSQLEmissions + unenforcedEmissions.size() + numPCEmissionMatches);
+        numEnforcedSQLEmissions + unenforcedEmissions.size()
+        + numPCEmissionMatches);
     log.info("Component Structure Valid?\t\t\t\t{}",
-      ComponentStructureVerifier.hasBeenVerified ? "YES" : "!!NO!!");
+        ComponentStructureVerifier.hasBeenVerified ? "YES" : "!!NO!!");
     log.info("Coverage Area Bounded?\t\t\t\t{}",
-      mismatchFlag ? "!!NO!!" : "YES");
+        mismatchFlag ? "!!NO!!" : "YES");
     log.info("Coverage Area (Start / End Lines):\tL_{}\t\tL_{}",
-      coverageAreaStartLineNbr, coverageAreaEndLineNbr);
-    log.info("\nNext unmatched emission in trace file: {}", getNextTraceEmission());
+        coverageAreaStartLineNbr, coverageAreaEndLineNbr);
+    log.info("\nNext unmatched emission in trace file: {}",
+        getNextTraceEmission());
   }
 }

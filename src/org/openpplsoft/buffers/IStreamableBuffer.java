61593bb3be8175149f11bf6a2513d189ad68c302
Matt Quinn <matt@mattjquinn.com>
2014-06-11 18:37:28 +0000

    IStreamableBuffer.java now passes checkstyle audit.

--- src/org/openpplsoft/buffers/IStreamableBuffer.java
+++ src/org/openpplsoft/buffers/IStreamableBuffer.java
@@ -7,8 +7,21 @@
 
 package org.openpplsoft.buffers;
 
+/**
+ * All PT buffer definitions that have a need to be streamable
+ * in the OPS runtime must implement this interface.
+ */
 public interface IStreamableBuffer {
+  /**
+   * Retrieves the next buffer in the read sequence.
+   * @return the next buffer in the read sequence, or null
+   *    if all child buffers have been read.
+   */
+  IStreamableBuffer next();
 
-  public abstract IStreamableBuffer next();
-  public abstract void resetCursors();
+  /**
+   * Resets the read cursors for this and all child buffers;
+   * call will be propagated to all children.
+   */
+  void resetCursors();
 }

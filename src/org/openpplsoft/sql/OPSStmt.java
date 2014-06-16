d3fbff2e4d146fd7ffbdbb228a56c0477b81b907
Matt Quinn <matt@mattjquinn.com>
2014-06-15 21:26:27 -0700

    Partial clean-up of OPSStmt.java.

--- src/org/openpplsoft/sql/OPSStmt.java
+++ src/org/openpplsoft/sql/OPSStmt.java
@@ -8,43 +8,60 @@
 package org.openpplsoft.sql;
 
 import java.sql.Connection;
-import java.util.HashMap;
 import java.sql.PreparedStatement;
-import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import org.openpplsoft.runtime.*;
-import org.apache.logging.log4j.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.openpplsoft.runtime.*;
+
+/**
+ * Used for all SQL emissions emitted by
+ * the OPS runtime.
+ */
 public class OPSStmt extends SQLStmt {
 
-  private static Logger log = LogManager.getLogger(OPSStmt.class.getName());
-  private static int stmtCounter = 0;
+  private static Logger log =
+      LogManager.getLogger(OPSStmt.class.getName());
 
-  public OPSStmt(String sql) {
+  private static int stmtCounter;
+
+  /**
+   * @param sql the sql statement to be executed
+   */
+  public OPSStmt(final String sql) {
     super(sql.trim());
   }
 
-  public PreparedStatement generateEnforcedPreparedStmt(Connection conn) {
-    PreparedStatement pstmt = generatePreparedStmt(conn);
+  public PreparedStatement generateEnforcedPreparedStmt(
+      final Connection conn) {
+    final PreparedStatement pstmt = this.generatePreparedStmt(conn);
     TraceFileVerifier.enforceEmission(this);
     return pstmt;
   }
 
-  public PreparedStatement generateUnenforcedPreparedStmt(Connection conn) {
-    PreparedStatement pstmt = generatePreparedStmt(conn);
+  public PreparedStatement generateUnenforcedPreparedStmt(
+      final Connection conn) {
+    final PreparedStatement pstmt = this.generatePreparedStmt(conn);
     TraceFileVerifier.submitUnenforcedEmission(this);
     return pstmt;
   }
 
-  private PreparedStatement generatePreparedStmt(Connection conn) {
+  private PreparedStatement generatePreparedStmt(
+      final Connection conn) {
     try {
-      PreparedStatement pstmt = conn.prepareStatement(this.sql);
-      for(Map.Entry<Integer, String> cursor : this.bindVals.entrySet()) {
+      final PreparedStatement pstmt = conn.prepareStatement(this.sql);
+      for (Map.Entry<Integer, String> cursor : this.bindVals.entrySet()) {
         pstmt.setString(cursor.getKey(), cursor.getValue());
       }
       return pstmt;
-    } catch(java.sql.SQLException sqle) {
+    } catch (final java.sql.SQLException sqle) {
       log.fatal(sqle.getMessage(), sqle);
       System.exit(ExitCode.FAILED_TO_CREATE_PSTMT_FROM_CONN.getCode());
     }

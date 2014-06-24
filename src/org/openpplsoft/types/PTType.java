cab113de27bac30613c16b5efc36b9a8ed2ab79a
Matt Quinn <matt@mattjquinn.com>
2014-06-23 18:14:59 -0700

    PTType.java now passes checkstyle audit.

--- src/org/openpplsoft/types/PTType.java
+++ src/org/openpplsoft/types/PTType.java
@@ -7,103 +7,186 @@
 
 package org.openpplsoft.types;
 
-import java.util.*;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openpplsoft.pt.*;
 import org.openpplsoft.pt.peoplecode.*;
 
+/**
+ * Root parent class for all PeopleTools type implementations.
+ */
 public abstract class PTType {
+
+  private static Map<String, PTType> sentinelCache;
 
   private Type type;
   private EnumSet<TFlag> flags;
   private boolean sentinelFlag;
 
-  private static Map<String, PTType> sentinelCache;
-
   static {
     sentinelCache = new HashMap<String, PTType>();
   }
 
-  public abstract boolean typeCheck(PTType a);
-
-  protected PTType(Type t) {
+  /**
+   * Creates a new type object corresponding to the enumerated
+   * value provided. Can only be called by concrete subclasses.
+   * @param t the enumerated flag of the type to create
+   */
+  protected PTType(final Type t) {
     this.type = t;
     this.flags = EnumSet.noneOf(TFlag.class);
   }
 
-  protected static boolean isSentinelCached(String key) {
+  /**
+   * Determines if the provided type corresponds to this
+   * object's type. This is implementation specific and thus
+   * must be implemented by subclasses.
+   * @param a the type object to check against
+   * @return whether the provided object has the same type
+   *    as this one.
+   */
+  public abstract boolean typeCheck(PTType a);
+
+  /**
+   * Determines if a sentinel type object has been cached
+   * yet.
+   * @param key the key of the sentinel type object to check
+   *   for in the cache
+   * @return whether the sentinel type object exists in the
+   *    cache
+   */
+  protected static boolean isSentinelCached(final String key) {
     return sentinelCache.containsKey(key);
   }
 
-  protected static PTType getCachedSentinel(String key) {
+  /**
+   * Returns a cached sentinel type object.
+   * @param key the key of the sentinel type object to get
+   *    the cache
+   * @return the cached sentinel type object requested
+   */
+  protected static PTType getCachedSentinel(final String key) {
     return sentinelCache.get(key);
   }
 
-  protected static void cacheSentinel(PTType sentinelObj, String cacheKey) {
+  /**
+   * Adds a sentinel type object to the cache.
+   * @param sentinelObj the sentinel type object to cache
+   * @param cacheKey the key to store the sentinel type
+   *    object under in the cache
+   */
+  protected static void cacheSentinel(final PTType sentinelObj,
+      final String cacheKey) {
     sentinelObj.setSentinel();
     sentinelCache.put(cacheKey, sentinelObj);
   }
 
-  protected static void clone(PTType src, PTType dest) {
-    if(src.type != dest.type) {
-      throw new EntDataTypeException("Attempted to clone PTType objects " +
-        "with different type enum flags (" + src.type + " to " + dest.type + ")");
+  /**
+   * Clones a source type to a destination type object. The
+   * source and destination must have the same enumerated type
+   * flag.
+   * @param src the source type object to clone from
+   * @param dest the destination type object to clone to
+   */
+  protected static void clone(final PTType src,
+      final PTType dest) {
+    if (src.type != dest.type) {
+      throw new EntDataTypeException("Attempted to clone PTType objects "
+          + "with different type enum flags (" + src.type + " to "
+          + dest.type + ")");
     }
     dest.setType(src.getType());
     dest.setFlags(src.getFlags());
 
-    if(src instanceof PTArray) {
-      ((PTArray)dest).dimensions = ((PTArray)src).dimensions;
-      ((PTArray)dest).baseType = ((PTArray)src).baseType;
+    if (src instanceof PTArray) {
+      ((PTArray) dest).dimensions = ((PTArray) src).dimensions;
+      ((PTArray) dest).baseType = ((PTArray) src).baseType;
     }
   }
 
+  /**
+   * Marks the instance type object as read-only.
+   * @return the instance itself (allows chained methods)
+   */
   public PTType setReadOnly() {
-    if(this.isSentinel()) {
-      throw new EntDataTypeException("Encountered illegal attempt to " +
-        "make a sentinel PTType readonly.");
+    if (this.isSentinel()) {
+      throw new EntDataTypeException("Encountered illegal attempt to "
+          + "make a sentinel PTType readonly.");
     }
     this.flags.add(TFlag.READONLY);
     return this;
   }
 
+  /**
+   * Check if the instance is a sentinel type object.
+   * @return whether or not the instance is a sentinel
+   */
   protected boolean isSentinel() {
     return this.sentinelFlag;
   }
 
+  /**
+   * Mark the instance type object as a sentinel.
+   * @return the instance itself (allows chained methods)
+   */
   private PTType setSentinel() {
     this.sentinelFlag = true;
     return this;
   }
 
+  /**
+   * Get the enumerated type of this instance.
+   * @return the enumerated type of this instance
+   */
   public Type getType() {
     return this.type;
   }
 
-  private void setType(Type t) {
-    if(this.isSentinel()) {
-      throw new EntDataTypeException("Encountered illegal attempt to " +
-        "set the type of a sentinel PTType.");
+  /**
+   * Set the enumerated type of this instance.
+   * @param t the enumerated type to use
+   */
+  private void setType(final Type t) {
+    if (this.isSentinel()) {
+      throw new EntDataTypeException("Encountered illegal attempt to "
+          + "set the type of a sentinel PTType.");
     }
     this.type = t;
   }
 
+  /**
+   * Get the set of type flags for this instance.
+   * @return the set of enumerated type flags for this
+   *   instance
+   */
   protected EnumSet<TFlag> getFlags() {
     return this.flags;
   }
 
-  private void setFlags(EnumSet<TFlag> fSet) {
-    if(this.isSentinel()) {
-      throw new EntDataTypeException("Encountered illegal attempt to " +
-        "set the flags of a sentinel PTType.");
+  /**
+   * Set the type flags for this instance object.
+   * @param fSet the set of flags to use for this type object
+   */
+  private void setFlags(final EnumSet<TFlag> fSet) {
+    if (this.isSentinel()) {
+      throw new EntDataTypeException("Encountered illegal attempt to "
+          + "set the flags of a sentinel PTType.");
     }
     this.flags = fSet.clone();
   }
 
+  @Override
   public String toString() {
-    StringBuilder b = new StringBuilder();
-    if(this.isSentinel()) { b.append("(SENTINEL)"); }
+    final StringBuilder b = new StringBuilder();
+    if (this.isSentinel()) {
+      b.append("(SENTINEL)");
+    }
     b.append(this.type);
-    if(this.flags.size() > 0) {b.append(this.flags); }
+    if (this.flags.size() > 0) {
+      b.append(this.flags);
+    }
     return b.toString();
   }
 }

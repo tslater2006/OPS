29eb99f27927ceb017a6296734e786784705a93e
Matt Quinn <matt@mattjquinn.com>
2014-06-28 16:00:35 +0000

    RecordLiteral.java now passes checkstyle audit.

--- src/org/openpplsoft/types/PTRecordLiteral.java
+++ src/org/openpplsoft/types/PTRecordLiteral.java
@@ -7,87 +7,123 @@
 
 package org.openpplsoft.types;
 
+import java.util.List;
+
 import org.openpplsoft.pt.*;
-import java.util.*;
 import org.openpplsoft.runtime.*;
 
+/**
+ * Represents a PeopleTools record literal.
+ */
 public final class PTRecordLiteral extends PTObjectType {
 
   private static Type staticTypeFlag = Type.REC_LITERAL;
-  public String RECNAME;
+
+  private String ptRECNAME;
   private Record recDefn;
 
+  /**
+   * Creates a new record literal object that isn't
+   * attached to a record defn; can only be called by
+   * an internal method.
+   */
   protected PTRecordLiteral() {
     super(staticTypeFlag);
   }
 
-  protected PTRecordLiteral(Record r) {
+  /**
+   * Creates a new record literal that is attached to a
+   * specific record defn; can only be called by an internal
+   * method.
+   * @param r the record defn to attach
+   */
+  protected PTRecordLiteral(final Record r) {
     super(staticTypeFlag);
-    this.RECNAME = r.RECNAME;
+    this.ptRECNAME = r.RECNAME;
     this.recDefn = r;
   }
 
-  /*
+  /**
    * Dot accesses on record field literals must
    * always return the appropriate FieldLiteral,
    * assuming the value for s is a valid field on
    * the underlying record.
+   * @param s the name of the property
+   * @return the FieldLiteral corresponding to {@code s}
    */
-  public PTType dotProperty(String s) {
-    List<RecordField> rfList = this.recDefn.getExpandedFieldList();
-    for(RecordField rf : rfList) {
-      if(rf.FIELDNAME.equals(s)) {
-        return PTFieldLiteral.getSentinel().alloc(this.RECNAME, s);
+  @Override
+  public PTType dotProperty(final String s) {
+    final List<RecordField> rfList = this.recDefn.getExpandedFieldList();
+    for (RecordField rf : rfList) {
+      if (rf.FIELDNAME.equals(s)) {
+        return PTFieldLiteral.getSentinel().alloc(this.ptRECNAME, s);
       }
     }
 
-    throw new OPSVMachRuntimeException("Unable to resolve s=" +
-      s + " to a field on the PTRecordLiteral for record " +
-      this.RECNAME);
+    throw new OPSVMachRuntimeException("Unable to resolve s="
+        + s + " to a field on the PTRecordLiteral for record "
+        + this.ptRECNAME);
   }
 
-  public Callable dotMethod(String s) {
+  @Override
+  public Callable dotMethod(final String s) {
     return null;
   }
 
-  public PTPrimitiveType castTo(PTPrimitiveType t) {
+  @Override
+  public PTPrimitiveType castTo(final PTPrimitiveType t) {
     throw new EntDataTypeException("castTo() has not been implemented.");
   }
 
-  public boolean typeCheck(PTType a) {
-    return (a instanceof PTRecordLiteral &&
-        this.getType() == a.getType());
+  @Override
+  public boolean typeCheck(final PTType a) {
+    return (a instanceof PTRecordLiteral
+        && this.getType() == a.getType());
   }
 
+  /**
+   * Retrieves a sentinel record literal object from the cache,
+   * or creates it if it doesn't exist.
+   * @return the sentinel object
+   */
   public static PTRecordLiteral getSentinel() {
 
     // If the sentinel has already been cached, return it immediately.
-    String cacheKey = getCacheKey();
-    if(PTType.isSentinelCached(cacheKey)) {
-      return (PTRecordLiteral)PTType.getCachedSentinel(cacheKey);
+    final String cacheKey = getCacheKey();
+    if (PTType.isSentinelCached(cacheKey)) {
+      return (PTRecordLiteral) PTType.getCachedSentinel(cacheKey);
     }
 
     // Otherwise, create a new sentinel type and cache it before returning it.
-    PTRecordLiteral sentinelObj = new PTRecordLiteral();
+    final PTRecordLiteral sentinelObj = new PTRecordLiteral();
     PTType.cacheSentinel(sentinelObj, cacheKey);
     return sentinelObj;
   }
 
-  public PTRecordLiteral alloc(Record recDefn) {
-    PTRecordLiteral newObj = new PTRecordLiteral(recDefn);
+  /**
+   * Allocates a new record literal object with a record defn
+   * attached.
+   * @param r the record defn to attach
+   * @return the newly allocated record literal object
+   */
+  public PTRecordLiteral alloc(final Record r) {
+    final PTRecordLiteral newObj = new PTRecordLiteral(r);
     PTType.clone(this, newObj);
     return newObj;
   }
 
+  /**
+   * Retrieves the cache key for this record literal.
+   * @return the record literal's cache key
+   */
   private static String getCacheKey() {
-    StringBuilder b = new StringBuilder(staticTypeFlag.name());
-    return b.toString();
+    return new StringBuilder(staticTypeFlag.name());
   }
 
   @Override
   public String toString() {
-    StringBuilder b = new StringBuilder(super.toString());
-    b.append(",RECNAME=").append(this.RECNAME);
+    final StringBuilder b = new StringBuilder(super.toString());
+    b.append(",ptRECNAME=").append(this.ptRECNAME);
     return b.toString();
   }
 }

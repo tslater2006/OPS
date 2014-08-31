fc3ee7cd4d04cd4bbf5870d922b30aae6a1d2d1d
Matt Quinn <matt@mattjquinn.com>
2014-08-30 20:22:04 -0700

    No need to override typeCheck on PTRecordTypeConstraint.

--- src/org/openpplsoft/types/PTRecordTypeConstraint.java
+++ src/org/openpplsoft/types/PTRecordTypeConstraint.java
@@ -27,17 +27,18 @@ public class PTRecordTypeConstraint extends PTTypeConstraint<PTRecord> {
         + "without providing associated record defn is illegal.");
   }
 
+ /**
+  * Allocates a new record object with an attached record defn.
+  * Allocated records must have an associated record defn in order
+  * to determine the type of the value enclosed within them. However, this
+  * defn is not part of the type itself; a Record variable can be assigned
+  * any Record object, regardless of its underlying record defn (this is why
+  * the typeCheck method is not overriden by this class, there's no need to do so).
+  * @param recDefn the record defn to attach
+  * @return the newly allocated record object
+  */
   public PTRecord alloc(final Record recDefn) {
     return new PTRecord(this, recDefn);
-  }
-
-  @Override
-  public boolean typeCheck(PTType a) {
-    throw new EntDataTypeException("TODO: Override typeCheck on "
-        + "PTRecordTypeConstraint (may not be necessary actually).");
-/*    log.debug("Constraint typecheck: underlying class is {}, a is {}",
-      this.underlyingClass, a.getClass());
-    return (this.underlyingClass == a.getClass());*/
   }
 
   @Override

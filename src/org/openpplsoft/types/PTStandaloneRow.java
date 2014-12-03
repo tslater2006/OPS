a00120a00ea973a950714ba554771a9b06f9a870
Matt Quinn <matt@mattjquinn.com>
2014-12-03 08:45:29 -0800

    Removing unnecessary code.

--- src/org/openpplsoft/types/PTStandaloneRow.java
+++ src/org/openpplsoft/types/PTStandaloneRow.java
@@ -124,163 +124,34 @@ public final class PTStandaloneRow extends PTRow {
     }
   }
 
-  // Used to register record defns that have an asscoiated buffer
-  // (i.e., for records in the component buffer).
+  /**
+   * MQUINN 12-03-2014 : Remove after split.
+   */
   public void registerRecordDefn(final RecordBuffer recBuffer) {
-    throw new OPSVMachRuntimeException("MQUINN 11-30-2014 : Disabling for split.");
-/*
-    final Record recDefn = recBuffer.getRecDefn();
-
-    // Only register the record defn if it hasn't already been registered.
-    if (!this.registeredRecordDefns.contains(recDefn)) {
-      this.registeredRecordDefns.add(recDefn);
-      this.recordMap.put(recDefn.RECNAME,
-          new PTRecordTypeConstraint().allocBufferRecord(this, recBuffer));
-    }*/
   }
-
   public void registerChildScrollDefn(final ScrollBuffer childScrollDefn) {
-    throw new OPSVMachRuntimeException("MQUINN 11-30-2014 : Disabling for split.");
-  /*  if (this.registeredChildScrollDefns.containsKey(
-        childScrollDefn.getPrimaryRecName())) {
-      throw new OPSVMachRuntimeException("Halting on call to register child "
-          + "scroll defn with a primary record name that has already been registerd; "
-          + "registering it again would overwrite a potentially different defn.");
-    } else {
-      this.registeredChildScrollDefns.put(
-          childScrollDefn.getPrimaryRecName(), childScrollDefn);
-      this.rowsetMap.put(childScrollDefn.getPrimaryRecName(),
-          childScrollDefn.allocRowset(this));
-    }*/
   }
-
   public void emitScrolls(final String indent) {
-
-    int scrollLevel = Math.max(0, this.determineScrollLevel());
-    TraceFileVerifier.submitEnforcedEmission(new BeginLevel(
-        scrollLevel, this.getIndexOfThisRowInParentRowset(), 1, 1, 0,
-        this.registeredChildScrollDefns.size(),
-        // All non-level 0 rows seem to have these flags enabled.
-        (scrollLevel != 0), (scrollLevel != 0),
-        this.registeredRecordDefns.size()));
-
-    for (Map.Entry<String, PTRecord> entry : this.recordMap.entrySet()) {
-      entry.getValue().emitRecInScroll();
-    }
-
-    for (Map.Entry<String, PTRecord> entry : this.recordMap.entrySet()) {
-      entry.getValue().emitScrolls(indent + "  ");
-    }
-
-    for (Map.Entry<String, PTRowset> entry : this.rowsetMap.entrySet()) {
-      entry.getValue().emitScrolls(indent + "  ");
-    }
   }
-
   public void fireEvent(final PCEvent event,
       final FireEventSummary fireEventSummary) {
-
-    // Fire event on each record in this row.
-    for (Map.Entry<String, PTRecord> entry : this.recordMap.entrySet()) {
-      entry.getValue().fireEvent(event, fireEventSummary);
-    }
-
-    // Fire event on each rowset in this row.
-    for (Map.Entry<String, PTRowset> entry : this.rowsetMap.entrySet()) {
-      entry.getValue().fireEvent(event, fireEventSummary);
-    }
   }
-
   public void runFieldDefaultProcessing(
       final FieldDefaultProcSummary fldDefProcSummary) {
-
-    // Run field default processing on each record in this row.
-    for (Map.Entry<String, PTRecord> entry : this.recordMap.entrySet()) {
-      entry.getValue().runFieldDefaultProcessing(fldDefProcSummary);
-    }
-
-    // Run field default processing on each rowset in this row.
-    for (Map.Entry<String, PTRowset> entry : this.rowsetMap.entrySet()) {
-      entry.getValue().runFieldDefaultProcessing(fldDefProcSummary);
-    }
   }
-
   public PTRecord resolveContextualCBufferRecordReference(final String recName) {
-    if (this.recordMap.containsKey(recName)) {
-      return this.recordMap.get(recName);
-    } else if (this.parentRowset != null) {
-      return this.parentRowset.resolveContextualCBufferRecordReference(recName);
-    }
     return null;
   }
-
   public PTReference<PTField> resolveContextualCBufferRecordFieldReference(
-      final String recName, final String fieldName) {
-    if (this.recordMap.containsKey(recName)
-        && this.recordMap.get(recName).hasField(fieldName)) {
-      return this.recordMap.get(recName).getFieldRef(fieldName);
-    } else if (this.parentRowset != null) {
-      return this.parentRowset.resolveContextualCBufferRecordFieldReference(
-          recName, fieldName);
-    }
+      String recName, String fldName) {
     return null;
   }
-
   public PTRowset resolveContextualCBufferScrollReference(
       final PTScrollLiteral scrollName) {
-    if (this.rowsetMap.containsKey(scrollName.read())) {
-      return this.rowsetMap.get(scrollName.read());
-    } else if (this.parentRowset != null) {
-      return this.parentRowset.resolveContextualCBufferScrollReference(scrollName);
-    }
     return null;
   }
-
   public void generateKeylist(
       final String fieldName, final Keylist keylist) {
-
-    // Can't continue if no parent rowset (scroll) exists.
-    if (this.parentRowset == null) {
-      return;
-    }
-
-    // If the parent rowset does not have a scroll defn, it may be the root
-    // PTRowset representing the ComponentBuffer as a whole. If this is the case,
-    // the entire search record (primary record for that rowset) should be searched.
-    if (this.parentRowset.getCBufferScrollDefn() == null) {
-      if (this.parentRowset == ComponentBuffer.getCBufferRowset()) {
-
-        final PTRecord searchRecord = this.recordMap.get(
-            ComponentBuffer.getComponentDefn().getSearchRecordName());
-
-        if (searchRecord.hasField(fieldName)) {
-          final PTField fld = searchRecord.getFieldRef(fieldName).deref();
-          keylist.add(fld);
-        }
-      } else {
-        throw new OPSVMachRuntimeException("Parent rowset has null scroll defn, but it is not "
-            + "of the root PTRowset object representing the component buffer as expected.");
-      }
-      return;
-    }
-
-    /*
-     * We must access the buffer definitions, not the raw records themselves,
-     * as not all record fields may be in the component buffer for any given record.
-     */
-    for (final RecordBuffer recBuf
-        : this.parentRowset.getCBufferScrollDefn().getOrderedRecBuffers()) {
-      final RecordFieldBuffer rfBuf = recBuf.getRecordFieldBuffer(fieldName);
-
-      if (rfBuf != null) {
-        final PTField fld = this.recordMap.get(
-            rfBuf.getRecDefn().RECNAME).getFieldRef(rfBuf.getRecFldDefn()
-                .FIELDNAME).deref();
-        keylist.add(fld);
-      }
-    }
-
-    this.parentRowset.generateKeylist(fieldName, keylist);
   }
 
   public PTRowset getParentRowset() {
